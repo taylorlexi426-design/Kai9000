4ba50f7b60e04be3867b0b5c8c9ee8d91bd1d890
taylorlexi426-design <taylor.lexi.426@gmail.com>
2026-08-30 06:33:13 -0400

    Use in-memory mock database to get server running

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,35 +1,30 @@
-const { Sequelize } = require('sequelize');
 const logger = require('../utils/logger');
-const path = require('path');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, 'kai9000.db'),
-  logging: false,
-  dialectOptions: {
-    timeout: 20000,
-  },
-});
-
-const db = {
-  sequelize,
-  Sequelize,
+// Mock in-memory database for development
+const mockDb = {
+  users: [],
+  tasks: [],
+  conversations: [],
+  messages: [],
 };
 
-// Import models
-db.User = require('./models/User')(sequelize, Sequelize);
-db.Task = require('./models/Task')(sequelize, Sequelize);
-db.Conversation = require('./models/Conversation')(sequelize, Sequelize);
-db.Message = require('./models/Message')(sequelize, Sequelize);
-
-// Define associations
-db.User.hasMany(db.Task, { foreignKey: 'userId' });
-db.Task.belongsTo(db.User, { foreignKey: 'userId' });
-
-db.Task.hasMany(db.Conversation, { foreignKey: 'taskId' });
-db.Conversation.belongsTo(db.Task, { foreignKey: 'taskId' });
-
-db.Conversation.hasMany(db.Message, { foreignKey: 'conversationId' });
-db.Message.belongsTo(db.Conversation, { foreignKey: 'conversationId' });
+const db = {
+  sequelize: {
+    authenticate: async () => {
+      logger.info('Mock database connected');
+      return true;
+    },
+    sync: async () => {
+      logger.info('Mock database synced');
+      return true;
+    },
+  },
+  Sequelize: {},
+  // Mock models
+  User: { findAll: () => mockDb.users, create: (data) => mockDb.users.push(data) },
+  Task: { findAll: () => mockDb.tasks, create: (data) => mockDb.tasks.push(data) },
+  Conversation: { findAll: () => mockDb.conversations, create: (data) => mockDb.conversations.push(data) },
+  Message: { findAll: () => mockDb.messages, create: (data) => mockDb.messages.push(data) },
+};
 
 module.exports = db;
